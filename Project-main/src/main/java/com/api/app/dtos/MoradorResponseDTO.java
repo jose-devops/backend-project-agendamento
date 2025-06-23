9e15597adb7360674e40cebf027e7837408f4304
José Roberto <joseroberto.devops@gmail.com>
2025-06-23 01:33:23 -0300

    Feita a implementação das classes para testes unitarios

--- Project-main/src/main/java/com/api/app/dtos/MoradorResponseDTO.java
+++ Project-main/src/main/java/com/api/app/dtos/MoradorResponseDTO.java
@@ -1,38 +1,30 @@
 package com.api.app.dtos;
-import com.api.app.models.enums.Especialidade;
+
+import com.api.app.models.MoradorModel;
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Data
-public class PrestadorResponseDTO {
+public class MoradorResponseDTO {
 
     private Long id;
-
-    private String razao;
-
-    private String cpfCnpj;
-
+    private String nome;
+    private String cpf;
+    private LocalDate dataAniversario;
     private String telefonePrincipal;
-
     private String telefoneSecundario;
-
-    private String linkWhatsapp;
-
-    private Especialidade especialidade;
-
+    private String profissao;
+    private BigDecimal rendaMensal;
     private String observacao;
 
+    private String usuarioEmail;
+    private Long proprietarioId;
 
-    public PrestadorResponseDTO(Long id, String razao, String cpfCnpj, String telefonePrincipal, String telefoneSecundario,
-                                String linkWhatsapp, Especialidade especialidade, String observacao) {
-        this.id = id;
-        this.razao = razao;
-        this.cpfCnpj = cpfCnpj;
-        this.telefonePrincipal = telefonePrincipal;
-        this.telefoneSecundario = telefoneSecundario;
-        this.linkWhatsapp = linkWhatsapp;
-        this.especialidade = especialidade;
-        this.observacao = observacao;
 
-    }
+
+
+
+
 }
